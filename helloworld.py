e1cf78b3642b542dd85ad96c481b7882616af064
SriHari Bandi <bandishari@gmail.com>
2018-07-11 22:56:28 -0500

    commit - 2

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,17 @@
 print('Hello World!! Python Programming')
-def adding2Numbers(arg1: int, arg2: int):
+def adding2Numbers(arg1, arg2):
     return arg1 + arg2
 
+print(__name__)
+print(__package__)
 
 print(adding2Numbers(10,20))
+
+ages = [12,20,30,12,50]
+
+for age in ages:
+    print age
+
+
+for (counter, age) in enumerate(ages):
+    print(counter, age)
