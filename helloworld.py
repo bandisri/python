57aa557dbe8296dd8949cd10e2498341678550cf
SriHari Bandi <bandishari@gmail.com>
2018-07-14 16:27:09 -0500

    commit - 3

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,31 @@
-print('Hello World!! Python Programming')
-def adding2Numbers(arg1, arg2):
-    return arg1 + arg2
+# print('Hello World!! Python Programming')
+# def adding2Numbers(arg1, arg2):
+#     return arg1 + arg2
+#
+# print(__name__)
+# print(__package__)
+#
+# print(adding2Numbers(10,20))
+#
+# ages = [12,20,30,12,50]
+#
+# for age in ages:
+#     print age
+#
+#
+# for (counter, age) in enumerate(ages):
+#     print(counter, age)
 
-print(__name__)
-print(__package__)
+from datetime import date
+from datetime import time
+from datetime import datetime
 
-print(adding2Numbers(10,20))
+def main():
+    currentDate = date.today()
+    print("Current Date", currentDate)
+    print("Day", currentDate.day)
+    print("Month", currentDate.month)
+    print("Year", currentDate.year)
 
-ages = [12,20,30,12,50]
-
-for age in ages:
-    print age
-
-
-for (counter, age) in enumerate(ages):
-    print(counter, age)
+if __name__ == '__main__':
+    main()
